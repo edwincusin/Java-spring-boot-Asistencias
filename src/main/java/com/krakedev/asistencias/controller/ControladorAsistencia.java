bf4ca3afa4bcd229b624f2943ba1255ec98d677f
EDWINCSN <c.edwin32@hotmail.com>
2026-05-03 21:40:33 -0500

    EGCA: Elaboracion de controlador de asistencia | rest web services

---
+++ src/main/java/com/krakedev/asistencias/controller/ControladorAsistencia.java
@@ -0,0 +1,36 @@
+package com.krakedev.asistencias.controller;
+
+import java.util.ArrayList;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.krakedev.asistencias.dto.Asistencia;
+import com.krakedev.asistencias.dto.RegistroAsistencia;
+import com.krakedev.asistencias.servicios.ServicioAsistencia;
+
+@RestController
+@RequestMapping("/asistencia")
+public class ControladorAsistencia {
+	// Servicio inyectado por Spring (no usar new)
+	private final ServicioAsistencia servicioAsistencia;
+	// Constructor para inyección de dependencias
+	public ControladorAsistencia(ServicioAsistencia servicioAsistencia) {
+		this.servicioAsistencia=servicioAsistencia;
+	}
+	
+	// ENDPOINTS
+	
+	@PostMapping("/{cedula}")
+	public RegistroAsistencia registrarAsistencia(@PathVariable String cedula) {
+		return servicioAsistencia.registrarAsistencia(cedula);
+	}
+	
+	@GetMapping("/{cedula}")
+	public ArrayList<Asistencia> consultarAsistencia(@PathVariable String Cedula){
+		return servicioAsistencia.consultarAsistencia(Cedula);
+	}
+}
